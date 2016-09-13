d6484b6298562854e5e0c04bf94738163889a2ca
hilshire <william1993419@126.com>
2016-09-14 00:30:30 +0800

    database api

--- nodejs/db.js
+++ nodejs/db.js
@@ -2,18 +2,73 @@ var sqlite3 = require('sqlite3').verbose()
 
 exports.connect = function(callback) {
   callback = callback || () => {}
-  db = new sqlite3.Database('schema.sql',
+  db = new sqlite3.Database('hilshire.db',
     sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
     function(err) {
       if (err) {
-        utils.log('Fail on creating database' + err)
-        callback(err)
+        throw err
       } else
         callback()
     })
 }
 
-exports.disconnect = function(callback) {
+exports.disconnect = function() {
   db.close()
-  callback()
+}
+
+exports.blog = {
+  add: createRunFn('INSERT INTO blog VALUES(?, ?, ?, ?);'), 
+  del: createRunFn('DELETE FROM blog WHERE id = ?;'),
+  update: createRunFn('UPDATE blog SET title = ?, text = ?, type = ? WHERE id = ?;'),
+  query: createGetFn('SELECT * FROM blog WHERE id = ?;', 'get')
+}
+
+exports.project = {
+  add: createRunFn('INSERT INTO project VALUES(?, ?, ?, ?, ?);'), 
+  del: createRunFn('DELETE FROM project WHERE id = ?;'),
+  update: createRunFn('UPDATE project SET title = ?, describe = ?, address = ?, img = ? WHERE id = ?;'),
+  query: createGetFn('SELECT * FROM project WHERE id = ?;')
+}
+
+exports.tag = {
+  add: createRunFn('INSERT INTO tag VALUES(?, ?);'), 
+  del: createRunFn('DELETE FROM tag WHERE id = ?;'),
+  update: createRunFn('UPDATE tag SET type = ? WHERE id = ?;'),
+  query: createGetFn('SELECT * FROM tag WHERE id = ?;')
+}
+
+exports.blogTag = {
+  add: createRunFn('INSERT INTO blog_tag VALUES(?, ?);'), 
+  del: createRunFn('DELETE FROM blog_tag WHERE blog_id = ?, tag_id = ?;'),
+  update: createRunFn('UPDATE blog_tag SET title = ?, text = ?, type = ? WHERE id = ?;'),
+  queryByProject: createGetFn('SELECT * FROM blog_tag WHERE project_id = ?;'),
+  queryByTag: createGetFn('SELECT * FROM blog_tag WHERE tag_id = ?;'),
+}
+
+exports.projectTag = {
+  add: createRunFn('INSERT INTO project_tag VALUES(?, ?);'), 
+  del: createRunFn('DELETE FROM project_tag WHERE project_id = ? AND tag_id = ?;'),
+  update: createRunFn('UPDATE project_tag SET title = ?, text = ?, type = ? WHERE id = ?;'),
+  queryByProject: createGetFn('SELECT * FROM project_tag WHERE project_id = ?;'),
+  queryByTag: createGetFn('SELECT * FROM project_tag WHERE tag_id = ?;'),
+}
+
+// return a sqlite3 api function, default api is run
+function createRunFn(sql, method) {
+  return function(data, callback) {
+    console.log(db)
+    db[method || 'run'](sql, data, function(error) {
+      if (error) throw error
+          else callback()
+    })
+  }
+}
+
+function createGetFn(sql) {
+  return function(data, callback) {
+    db.get(sql, data, function(error, row) {
+      if (error) throw error
+          else callback(row)
+    })
+  }
 }
--- nodejs/server.js
+++ nodejs/server.js
@@ -1,3 +1,8 @@
-var db = require('./db.js')
+var hildb = require('./db.js')
 
-db.connect()
+hildb.connect()
+db.serialize(function() {
+    // hildb.tag.update(['test2', 1], function() {console.log('done')})
+    hildb.tag.query([1], function(row) {console.log(row)})
+})
+hildb.disconnect()
